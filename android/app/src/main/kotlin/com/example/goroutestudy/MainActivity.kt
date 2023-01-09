d65d9969f977c1d525b3c1a480a133b61dc91921
gehenna-yang <winer0707@naver.com>
2023-01-09 18:40:20 +0900

    project create

---
+++ android/app/src/main/kotlin/com/example/goroutestudy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.goroutestudy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/layout/default_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  const DefaultLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
