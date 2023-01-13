f80769f3d215c214c528e3815f6c9ffb391ba55f
gehenna-yang <winer0707@naver.com>
2023-01-13 15:56:20 +0900

    redirect

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:goroutestudy/layout/default_layout.dart';
+import 'package:goroutestudy/provider/auth_provider.dart';
 import 'package:goroutestudy/screen/2_screen.dart';
 import 'package:goroutestudy/screen/3_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -38,6 +40,18 @@ class HomeScreen extends StatelessWidget {
                 context.go('error');
               },
               child: Text('Error Screen')
+          ),
+          ElevatedButton(
+              onPressed: (){
+                context.go('/login');
+              },
+              child: Text('Login Screen')
+          ),
+          ElevatedButton(
+              onPressed: (){
+                ref.read(userProvider.notifier).logout();
+              },
+              child: Text('Logout')
           ),
         ],
       ),
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:goroutestudy/layout/default_layout.dart';
+import 'package:goroutestudy/provider/auth_provider.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
-      body: Column(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              ref.read(userProvider.notifier).login(name: 'Code Factory');
+            },
+            child: Text('Login')
+          ),
+        ],
+      ),
     );
   }
 }
